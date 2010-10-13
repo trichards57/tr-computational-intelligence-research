09846d41cc547f7f9d6c2521affbfd78476c8f3e
tony <tony@dc724949-f3d0-a60a-cc9a-1c2a6a8e5742>
2010-10-13 20:19:11 +0000

    Transferred vertical movement to VerticalMovePersonality. Implemented constant velocity flight profile.

--- trunk/LearningController.py
+++ trunk/LearningController.py
@@ -1,6 +1,7 @@
 from simulation import *
 from HoverFindPersonality import *
 from StopPersonality import *
+from VerticalMovePersonality import *
 from collections import deque
 
 # Setting this to a larger number makes the system more tolerant of residual
@@ -42,6 +43,9 @@ class LearningControl:
     hoverThrustFound = False
     # Used during initialisation to flag if the pod has come to a halt before the first maneuver.
     velocityZeroed = False
+    # Used during initialisation to flag if the pod has been position equidistant from the top and bottom walls sensed.
+    positionCenterStarted = False
+
     # Current personality that is running the pod
     personality = None
 
@@ -156,55 +160,71 @@ class LearningControl:
                 self.personality.process(state)
                 # We've found the hover thrust.
                 self.hoverThrustFound = True
-            else:
-                # Position centre vertically (ish) to give us room to move later.
-                # Assuming we're pointing straight up (everything else is screwed anyway if we aren't.)
-
+            elif self.positionCenterStarted == False:
+                # We need to position ourselves in the centre of the passage, vertically
+                # First work out the height of the passage.
                 distanceToTop = sensor[0].val
                 distanceToBottom = sensor[20].val
-                totalDistance = distanceToBottom + distanceToTop
-                # Distance to the point between half way between where the top sensor is reading and where the bottom sensor is reading
-                distanceToMiddle = totalDistance / 2 - distanceToBottom
+                passageHeight = distanceToTop + distanceToBottom
 
-                if self.distanceMeasured == False:
-                    # We've only just started moving. Work out how far to go.
-                    self.totalDistance = distanceToMiddle
+                bottomPosition = state.y + distanceToBottom
+                centerHeight = bottomPosition - (passageHeight / 2)
 
-                    # Get the largest possible moving force that
-                    # can be canceled in an equal amount of time
-                    # without rotating the pod.
-
-                    # Moving force can't be greater than the hover thrust,
-                    # otherwise the speeding and slowing thrusts will
-                    # unbalanced
-                    movingForce = self.hoverThrust
-                    # The sum of the moving force and the hover thrust
-                    # cannot be greater than 1, otherwise the thruster
-                    # will max out and the speeding and slowing thrusts
-                    # will be unbalanced
-                    if movingForce + self.hoverThrust > 1:
-                        movingForce = 1 - self.hoverThrust
-
-                    if self.totalDistance > 0:
-                        # We have to go up.
-                        self.accelThrust = fabs(movingForce) / self.accelThrustDiviser # Must be +ve
-                    else:
-                        # We have to go down.
-                        self.accelThrust = -fabs(movingForce) / self.accelThrustDiviser # Must be -ve
-
-                    self.distanceMeasured = True
-                    self.resetCancelVelocity()
-                    self.halfWay = False
-
-                if self.stopped == False:
-                    self.verticalMove(distanceToMiddle, state, accel, dt)
-                else:
-                    print "Stopped."
-                    if fabs(distanceToMiddle) > positionThreshold:
-                        # Start the destination finding process again, we're not quite close enough.
-                        self.distanceMeasured = False
-                        # But go a bit slower to be more accurate.
-                        self.accelThrustDiviser *= 2
+                # Make a VerticalMovePersonality the active personality
+                self.personality = VerticalMovePersonality(self.hoverThrust, state, centerHeight)
+                # Run the first cycle
+                self.personality.process(state)
+                # We have started to center.
+                self.positionCenterStarted = True
+#            else:
+#                # Position centre vertically (ish) to give us room to move later.
+#                # Assuming we're pointing straight up (everything else is screwed anyway if we aren't.)
+#
+#                distanceToTop = sensor[0].val
+#                distanceToBottom = sensor[20].val
+#                totalDistance = distanceToBottom + distanceToTop
+#                # Distance to the point between half way between where the top sensor is reading and where the bottom sensor is reading
+#                distanceToMiddle = totalDistance / 2 - distanceToBottom
+#
+#                if self.distanceMeasured == False:
+#                    # We've only just started moving. Work out how far to go.
+#                    self.totalDistance = distanceToMiddle
+#
+#                    # Get the largest possible moving force that
+#                    # can be canceled in an equal amount of time
+#                    # without rotating the pod.
+#
+#                    # Moving force can't be greater than the hover thrust,
+#                    # otherwise the speeding and slowing thrusts will
+#                    # unbalanced
+#                    movingForce = self.hoverThrust
+#                    # The sum of the moving force and the hover thrust
+#                    # cannot be greater than 1, otherwise the thruster
+#                    # will max out and the speeding and slowing thrusts
+#                    # will be unbalanced
+#                    if movingForce + self.hoverThrust > 1:
+#                        movingForce = 1 - self.hoverThrust
+#
+#                    if self.totalDistance > 0:
+#                        # We have to go up.
+#                        self.accelThrust = fabs(movingForce) / self.accelThrustDiviser # Must be +ve
+#                    else:
+#                        # We have to go down.
+#                        self.accelThrust = -fabs(movingForce) / self.accelThrustDiviser # Must be -ve
+#
+#                    self.distanceMeasured = True
+#                    self.resetCancelVelocity()
+#                    self.halfWay = False
+#
+#                if self.stopped == False:
+#                    self.verticalMove(distanceToMiddle, state, accel, dt)
+#                else:
+#                    print "Stopped."
+#                    if fabs(distanceToMiddle) > positionThreshold:
+#                        # Start the destination finding process again, we're not quite close enough.
+#                        self.distanceMeasured = False
+#                        # But go a bit slower to be more accurate.
+#                        self.accelThrustDiviser *= 2
 
         #control.up = self.hoverThrust + self.maneuverThrust
 
