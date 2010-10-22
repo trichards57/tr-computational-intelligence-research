72be9203fccddaf3c1eed9fb3b3448c1bf67769b
tony <tony@dc724949-f3d0-a60a-cc9a-1c2a6a8e5742>
2010-10-22 13:50:50 +0000

    Producing multi agent prototype. Began implementing wall following. Controller continuously dumps sensor output to sensorData.csv

---
+++ CSharp Multi-Agent/Multi-Agent Lab/MultiAgentLab/Classes/Agent.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace MultiAgentLab.Classes
+{
+    enum Directions
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    class Agent
+    {
+        private Point StartPosition;
+
+        public Point Position { get; set; }
+
+        public bool FoundEnd { get; set; }
+
+        private List<Point> pastRoute = new List<Point>();
+
+        private Random rand = new Random();
+
+        public Agent(Point startPosition)
+        {
+            Position = startPosition;
+            StartPosition = startPosition;
+            FoundEnd = false;
+        }
+
+        public void Process(Field field)
+        {
+            var x = (int)Position.X;
+            var y = (int)Position.Y;
+
+            var currentSquare = field[y][x];
+            var upSquare = field[y - 1][x];
+            var downSquare = field[y + 1][x];
+            var leftSquare = field[y][x - 1];
+            var rightSquare = field[y][x + 1];
+
+            if (currentSquare.Destination)
+            {
+                foreach (var p in pastRoute)
+                {
+                    field[(int)p.Y][(int)p.X].PheremoneLevel += 1.0;
+                }
+                Position = StartPosition;
+                pastRoute.Clear();
+            }
+            else
+            {
+
+                var totalWeight = upSquare.PheremoneLevel + downSquare.PheremoneLevel + leftSquare.PheremoneLevel + rightSquare.PheremoneLevel;
+                var randNum = rand.NextDouble() * totalWeight;
+
+                if (randNum < upSquare.PheremoneLevel)
+                    // Move up
+                    Position = upSquare.Position;
+                else if (randNum < (upSquare.PheremoneLevel + downSquare.PheremoneLevel))
+                    // Move down
+                    Position = downSquare.Position;
+                else if (randNum < (upSquare.PheremoneLevel + downSquare.PheremoneLevel + leftSquare.PheremoneLevel))
+                    // Move left
+                    Position = leftSquare.Position;
+                else
+                    // Move right
+                    Position = rightSquare.Position;
+
+                pastRoute.Add(currentSquare.Position);
+
+                currentSquare.PheremoneLevel += 0.001;
+            }
+        }
+    }
+}
---
+++ CSharp Multi-Agent/Multi-Agent Lab/MultiAgentLab/Window1.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MultiAgentLab.Classes;
+using System.Threading;
+
+namespace MultiAgentLab
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        List<Agent> agentsList = new List<Agent>();
+        Timer agentTicker;
+        Timer newAgentTicker;
+        int count = 0;
+        Field field;
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void TimerTick(object state)
+        {
+            var field = state as Field;
+            lock (agentsList)
+            {
+                foreach (var agent in agentsList)
+                    agent.Process(field);
+            }
+
+            foreach (var row in field)
+                foreach (var square in row)
+                    if (square.PheremoneLevel > 1)
+                        square.PheremoneLevel -= 0.00001;
+            count++;
+            if (count > 10000)
+            {
+                agentsList.Add(new Agent(field.StartPoint));
+                count = 0;
+            }
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            field = new Field(100, 100, @"C:\Users\Tony\Documents\Python Code\sensorData.csv");
+
+            //agentTicker = new Timer(TimerTick, DataContext, 10000, 100);
+        }
+    }
+}
