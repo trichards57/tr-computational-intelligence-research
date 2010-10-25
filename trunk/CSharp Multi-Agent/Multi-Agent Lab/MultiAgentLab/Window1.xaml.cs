af4bdaeaa626a42f67cdac148933f630c782c288
tony <tony@dc724949-f3d0-a60a-cc9a-1c2a6a8e5742>
2010-10-25 08:10:49 +0000

    Updated to use bitmap drawn instead of databinding.

--- trunk/CSharp Multi-Agent/Multi-Agent Lab/MultiAgentLab/Window1.xaml.cs
+++ trunk/CSharp Multi-Agent/Multi-Agent Lab/MultiAgentLab/Window1.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MultiAgentLab.Classes;
 using System.Threading;
+using Microsoft.Win32;
+using System.Windows.Threading;
+using System.Windows.Interop;
 
 namespace MultiAgentLab
 {
@@ -55,9 +58,89 @@ namespace MultiAgentLab
 
         private void WindowLoaded(object sender, RoutedEventArgs e)
         {
-            field = new Field(100, 100, @"C:\Users\Tony\Documents\Python Code\sensorData.csv");
+        }
 
-            //agentTicker = new Timer(TimerTick, DataContext, 10000, 100);
+        private void LoadMapClick(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new OpenFileDialog();
+            fileDialog.AddExtension = false;
+            fileDialog.CheckFileExists = true;
+            fileDialog.CheckPathExists = true;
+            fileDialog.DefaultExt = "csv";
+            fileDialog.DereferenceLinks = true;
+            fileDialog.Filter = "Comma Seperated Variables (*.csv)|*.csv|All Files (*.*)|*.*";
+            fileDialog.Multiselect = false;
+            fileDialog.ShowReadOnly = false;
+            fileDialog.Title = "Load Sensor Data";
+            fileDialog.ValidateNames = true;
+
+            var result = fileDialog.ShowDialog(this);
+
+            if (result == true)
+            {
+                var fieldWidth = int.Parse(MapWidthTextBlock.Text);
+                var fieldHeight = int.Parse(MapHeightTextBlock.Text);
+
+                Cursor = Cursors.Wait;
+                LoadMapButton.IsEnabled = false;
+                MapWidthTextBlock.IsEnabled = false;
+                MapHeightTextBlock.IsEnabled = false;
+
+                var worker = new Thread(LoadMap);
+                worker.Start(new LoadMapData { FileName = fileDialog.FileName, MapSize = new Size(fieldWidth, fieldHeight) });
+            }
+        }
+
+        private struct LoadMapData
+        {
+            public Size MapSize;
+            public string FileName;
+        }
+
+        private void LoadMapComplete()
+        {
+            ResetMapButton.IsEnabled = true;
+            LoadMapButton.IsEnabled = true;
+            MapWidthTextBlock.IsEnabled = true;
+            MapHeightTextBlock.IsEnabled = true;
+            Cursor = Cursors.Arrow;
+        }
+
+        private void LoadMap(object fileName)
+        {
+            var data = (LoadMapData)fileName;
+           
+            field = new Field((int)data.MapSize.Width, (int)data.MapSize.Height, data.FileName);
+
+            DrawMap();
+
+            Dispatcher.Invoke(DispatcherPriority.Normal, new Action(LoadMapComplete));
+        }
+
+        private void DrawMap()
+        {
+            var picture = new System.Drawing.Bitmap(field.Count * 10, field.First().Count * 10);
+            var graphics = System.Drawing.Graphics.FromImage(picture);
+
+            lock (field)
+            {
+                for (var y = 0; y < field.Count; y++)
+                {
+                    for (var x = 0; x < field[y].Count; x++)
+                    {
+                        var rect = new System.Drawing.Rectangle(x * 10, y * 10, 10, 10);
+                        var color = field[y][x].SquareColor;
+                        graphics.FillRectangle(new System.Drawing.SolidBrush(color), rect);
+                    }
+                }
+            }
+
+            Dispatcher.Invoke(new Action<System.Drawing.Bitmap>(UpdateMap), picture);
+        }
+
+        private void UpdateMap(System.Drawing.Bitmap image)
+        {
+            DataContext = Imaging.CreateBitmapSourceFromHBitmap(image.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
         }
     }
 }
