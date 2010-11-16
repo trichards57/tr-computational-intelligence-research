6d881cfb8e05bdbffad15fed93a32a543d4fd96b
tony <tony@dc724949-f3d0-a60a-cc9a-1c2a6a8e5742>
2010-11-16 01:09:15 +0000

    Continued development of CSharpSim.

--- CSharpSim/CSharpSim/Classes/World.cs
+++ CSharpSim/CSharpSim/Classes/World.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
 using System.IO;
-using System.Windows.Media;
+using System.Windows;
 
 namespace CSharpSim.Classes
 {
+    using System.Linq;
+
     class World
     {
         public List<Pod> Pods { get; set; }
@@ -16,40 +15,49 @@ namespace CSharpSim.Classes
         public int Ticks { get; set; }
         public bool Blind { get; set; }
 
-        public World(string filename, IEnumerable<Pod> pods)
+        public World()
         {
             Pods = new List<Pod>();
-            Pods.AddRange(pods);
             Walls = new List<Wall>();
+        }
 
+        public World(string filename, IEnumerable<Pod> pods) : this()
+        {
+            if (pods != null)
+                Pods.AddRange(pods);
+            
             using (var file = new StreamReader(File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.Read)))
             {
                 while (!file.EndOfStream)
                 {
-                    var l = file.ReadLine().Trim();
+                    var l = file.ReadLine();
+                    if (l == null) continue;
+                    l = l.Trim();
                     if (l.Length == 0 || l.StartsWith("#"))
                         continue;
 
                     var parts = l.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
 
-                    if (parts[0] == "wall")
+                    switch (parts[0])
                     {
-                        var wall = new Wall(parts[1]);
-                        wall.Read(file);
-                        Walls.Add(wall);
-                        Rectangle = Rect.Union(Rectangle, wall.Rectangle);
-                    }
-                    else if (parts[0] == "pod")
-                    {
-                        ReadPodPosition(file);
+                        case "wall":
+                            var wall = new Wall(parts[1]);
+                            wall.Read(file);
+                            Walls.Add(wall);
+                            Rectangle = Rect.Union(Rectangle, wall.Rectangle);
+                            break;
+                        case "pod":
+                            ReadPodPosition(file);
+                            break;
                     }
                 }
             }
         }
 
-        private void ReadPodPosition(StreamReader file)
+        private void ReadPodPosition(TextReader file)
         {
             var line = file.ReadLine();
+            if (line == null) return;
             var parts = line.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries);
 
             var x = float.Parse(parts[0]);
@@ -62,59 +70,44 @@ namespace CSharpSim.Classes
             }
         }
 
-        struct ClosestIntersect
+        internal struct ClosestIntersect
         {
-            public double tMin;
-            public Wall wallMin;
+            public double TMin;
+            public Wall WallMin;
         }
 
-        ClosestIntersect FindClosestIntersect(Point p0, Point p1)
+        public ClosestIntersect FindClosestIntersect(Point p0, Point p1)
         {
             var tMin = 2.0;
-            Wall wallMin = null; ;
+            Wall wallMin = null;
 
             foreach (var w in Walls)
             {
-                foreach (var s in w.Segments)
+                var min = tMin;
+                foreach (var res in from s in w.Segments let p2 = s.Point1 let p3 = s.Point2 select Utilities.Intersect(p0, p1, p2, p3) into res where res.t >= 0 && res.t <= 1.0 && res.s >= 0 && res.s <= 1.0 where res.t < min select res)
                 {
-                    var p2 = s.Point1;
-                    var p3 = s.Point2;
-
-                    var res = Interset(p0, p1, p2, p3);
-
-                    if (res.t >= -float.Epsilon && res.t <= 1.0+float.Epsilon && res.s >= -float.Epsilon && res.s <= 1.0 float.Epsilon)
-                    {
-                        if (res.t < tMin)
-                        {
-                            tMin = res.t;
-                            wallMin = w;
-                        }
-                    }
+                    tMin = res.t;
+                    wallMin = w;
                 }
             }
 
-            return new ClosestIntersect { tMin = tMin, wallMin = wallMin };
+            return new ClosestIntersect { TMin = tMin, WallMin = wallMin };
         }
 
-        struct IntersectResult
+        Wall CheckCollideWithWall(Point p0, Point p1)
         {
-            public double s;
-            public double t;
+            return p0 == p1 ? null : Walls.FirstOrDefault(w => w.Segments.Select(s => Utilities.Intersect(p0, p1, s.Point1, s.Point2)).Any(res => res.s >= 0 && res.s <= 1 && res.t >= 0 && res.t <= 1));
         }
 
-        IntersectResult Interset(Point p0, Point p1, Point p2, Point p3)
+        void Step(double dt)
         {
-            var line1 = p1 - p0;
-            var s2 = p3 - p2;
+            Ticks++;
 
-            var fact = (-s2.X * line1.Y + line1.X * s2.Y);
-    
-
-            return new IntersectResult
+            foreach (var p in Pods)
             {
-                s = (-line1.Y * (p0.X - p2.X) + line1.X * (p0.Y - p2.Y)) / fact,
-                t = (-s2.X * (p0.Y - p2.Y) - s2.Y * (p0.X - p2.X)) / fact
-            };
+                p.Step(dt, this);
+                p.UpdateSensors(this);
+            }
         }
     }
 }
--- CSharpSim/CSharpSim/Utilities.cs
+++ CSharpSim/CSharpSim/Utilities.cs
@@ -7,6 +7,12 @@ using System.IO;
 
 namespace CSharpSim
 {
+    struct IntersectResult
+    {
+        public double s;
+        public double t;
+    }
+
     static class Utilities
     {
         public static double Limit(double value, double max, double min)
@@ -20,7 +26,41 @@ namespace CSharpSim
 
         internal static List<Point> ReadPoints(StreamReader data)
         {
-            throw new NotImplementedException();
+            var points = new List<Point>();
+
+            while (true)
+            {
+                var l = data.ReadLine();
+                
+                if (string.IsNullOrWhiteSpace(l))
+                    return points;
+                if (l.StartsWith("#"))
+                    continue;
+
+                var parts = l.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+                for (var i = 0; i + 1 < parts.Length; i+=2)
+                {
+                    var x = double.Parse(parts[i]);
+                    var y = double.Parse(parts[i + 1]);
+
+                    points.Add(new Point(x, y));
+                }
+            }
+        }
+
+        public static IntersectResult Intersect(Point p0, Point p1, Point p2, Point p3)
+        {
+            var line1 = p1 - p0;
+            var line2 = p3 - p2;
+
+            var fact = (-line2.X * line1.Y + line1.X * line2.Y);
+
+
+            return new IntersectResult
+            {
+                s = (-line1.Y * (p0.X - p2.X) + line1.X * (p0.Y - p2.Y)) / fact,
+                t = (-line2.X * (p0.Y - p2.Y) - line2.Y * (p0.X - p2.X)) / fact
+            };
         }
     }
 }
